91b397b561094ebbbadeae77b258e90a13853845
shaharkr <shahar.kramer@gmail.com>
2021-11-09 21:14:00 +0200

    fixing PremiumAccount init

--- src/onlineShopping/PremiumAccount.java
+++ src/onlineShopping/PremiumAccount.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class PremiumAccount extends Account {
     ArrayList<Product> products;
-    public PremiumAccount(String id, String billing_address, boolean is_closed, Date open, Date close, Customer customer, ShoppingCart shop_cart, int balance) {
-        super(id, billing_address, is_closed, open, close, customer, shop_cart, balance);
+    public PremiumAccount(String id, Address billing_address, Customer customer) {
+        super(id, billing_address, customer);
     }
 }
